1f0ec0583095a529e13d6409b1734d563683b43c
lucky-morais <lucky_morais@hotmail.com>
2015-05-20 16:21:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
   <head>
     <title>Windows Azure + GitHub + php</title>
+    <link rel="stylesheet" type="text/css" 
+    	href="http://1000334.azurewebsites.net/estilos.css" />
   </head>
   <body>
+  <div id="myDiv">
+   <p>
     <?php
 	echo "Windows Azure + GitHub + PHP" ;
 	echo "<br />";
 	echo "<strong>Azure</strong> definitely rocks!" ;
     ?>
+    </p>
+    </div>
   </body>
 </html>
